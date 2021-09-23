10dac4fa2e07c7934306d52e917d91bbf970b028
MagnusKJensen <magnjensen@gmail.com>
2021-09-23 08:10:56 +0200

    Added directional animation to robot wheels in the new force-based controller.

--- Assets/Scripts/RobotForceController.cs
+++ Assets/Scripts/RobotForceController.cs
@@ -7,21 +7,33 @@ namespace Dora
 {
     public class RobotForceController : MonoBehaviour, SimulationUnit
     {
-        
         public Transform leftWheel;
         public Transform rightWheel;
 
-        [Range(1, 100)]
-        public int rorateForce = 10;
-        
-        [Range(1, 100)]
-        public int moveForce = 10;
+        [Range(1, 150)] public int rorateForce = 70;
+
+        [Range(1, 150)] public int moveForce = 95;
 
         private Vector3? lastLeftWheelPosition = null;
         private Vector3? lastRightWheelPosition = null;
-        
+
         public void SimUpdate(SimulationConfiguration config)
         {
+            // Calculate movement between current and last physics tick
+            var leftWheelDifferenceVector = leftWheel.transform.position - lastLeftWheelPosition ?? Vector3.zero;
+            var rightWheelDifferenceVector = rightWheel.transform.position - lastRightWheelPosition ?? Vector3.zero;
+
+            var forward = transform.forward;
+            var leftWheelMoveDirection = Vector3.Dot(forward, leftWheelDifferenceVector) < 0 ? -1f : 1f;
+            var rightWheelMoveDirection = Vector3.Dot(forward, rightWheelDifferenceVector) < 0 ? -1f : 1f;
+
+            // Animate rotating wheels to match movement of the robot
+            leftWheel.Rotate(new Vector3(80f * leftWheelMoveDirection* leftWheelDifferenceVector.magnitude, 0f, 0f));
+            rightWheel.Rotate(new Vector3(80f * rightWheelMoveDirection * rightWheelDifferenceVector.magnitude, 0f, 0f));
+
+            lastLeftWheelPosition = leftWheel.position;
+            lastRightWheelPosition = rightWheel.position;
+            
             if (Input.GetButton("Left"))
             {
                 GetComponent<Rigidbody>().AddForceAtPosition(transform.forward * rorateForce, leftWheel.position);
@@ -33,31 +45,20 @@ namespace Dora
                 GetComponent<Rigidbody>().AddForceAtPosition(transform.forward * -rorateForce, leftWheel.position);
                 GetComponent<Rigidbody>().AddForceAtPosition(transform.forward * rorateForce, rightWheel.position);
             }
-            
+
             if (Input.GetButton("Forward"))
             {
                 GetComponent<Rigidbody>().AddForceAtPosition(transform.forward * moveForce, leftWheel.position);
                 GetComponent<Rigidbody>().AddForceAtPosition(transform.forward * moveForce, rightWheel.position);
             }
-            
+
             if (Input.GetButton("Reverse"))
             {
                 GetComponent<Rigidbody>().AddForceAtPosition(transform.forward * -moveForce, leftWheel.position);
                 GetComponent<Rigidbody>().AddForceAtPosition(transform.forward * -moveForce, rightWheel.position);
             }
-            
-            var leftWheelDistance = (leftWheel.transform.position - lastLeftWheelPosition)?.magnitude;
-            var rightWheelDistance = (rightWheel.transform.position - lastRightWheelPosition)?.magnitude;
 
-            Debug.Log("Distance: " + lastRightWheelPosition.ToString() ?? "");
             
-            // TODO: Rotation direction
-            leftWheel.Rotate(new Vector3(100f * leftWheelDistance ?? 0f, 0f, 0f));
-            rightWheel.Rotate(new Vector3(80f * rightWheelDistance ?? 0f, 0f, 0f));
-
-            lastLeftWheelPosition = leftWheel.position;
-            lastRightWheelPosition = rightWheel.position;
         }
-        
     }
 }
