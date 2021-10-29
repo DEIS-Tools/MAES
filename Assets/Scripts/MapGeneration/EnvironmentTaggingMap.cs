b80df17f5da0a93ac200f7b9ed4c13d0d9ae647a
Magnus K Jensen <magnjensen@gmail.com>
2021-10-29 08:17:32 +0200

    Added template for EnvironmentTaggingMap.

---
+++ Assets/Scripts/MapGeneration/EnvironmentTaggingMap.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Dora.MapGeneration {
+    public class EnvironmentTaggingMap {
+
+
+        public class EnvironmentTag {
+
+            public readonly Vector2 WorldPosition;
+            public readonly object data;
+
+        }
+        
+    }
+}
