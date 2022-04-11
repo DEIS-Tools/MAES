4c8a0b997d3ecabe827dd516e5f80d6a496c3262
Philip Holler <philipholler94@gmail.com>
2022-04-11 10:30:55 +0200

    added workspace files for launch config

---
+++ Assets/maes-ros-slam-ws/src/unity_slam_example/launch/unity_slam_example.py
@@ -0,0 +1,46 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    package_name = 'unity_slam_example'
+    package_dir = get_package_share_directory(package_name)
+
+    return LaunchDescription({
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('ros_tcp_endpoint'), 'launch', 'endpoint.py')
+            ),
+        ),
+
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            arguments=['-d', os.path.join(package_dir, 'nav2_unity.rviz')],
+            parameters=[{'use_sim_time':True}]
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(package_dir, 'navigation_launch_maes.py')
+            ),
+            launch_arguments={
+                'use_sim_time': 'true'
+            }.items()
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('slam_toolbox'), 'launch', 'online_async_launch.py')
+            ),
+            launch_arguments={
+                'use_sim_time': 'true'
+            }.items()
+        )
+    })
---
+++ Assets/maes-ros-slam-ws/src/unity_slam_example/setup.py
@@ -0,0 +1,30 @@
+import glob
+import os
+
+from setuptools import setup
+
+package_name = 'unity_slam_example'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), [
+                                                'rviz/nav2_unity.rviz',
+                                                'launch/unity_slam_example.py',
+                                                'launch/unity_viz_example.py',
+                                                'launch/navigation_launch_maes.py',
+                                                'params/nav2_params_maes.yaml'
+                                                ])
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Unity Robotics',
+    maintainer_email='unity-robotics@unity3d.com',
+    description='Unity Robotics Nav2 SLAM Example',
+    license='Apache 2.0',
+    tests_require=['pytest']
+)
