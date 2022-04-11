d155921ccd52f0994137d222a760f1996963a1e6
Philip Holler <philipholler94@gmail.com>
2022-04-11 12:09:52 +0200

    Multiple robots now possible through launch argument

--- Assets/maes-ros-slam-ws/src/unity_slam_example/setup.py
+++ Assets/maes-ros-slam-ws/src/unity_slam_example/setup.py
@@ -13,18 +13,22 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), [
+                                                'rviz/nav2_namespaced_view.rviz',
                                                 'rviz/nav2_unity.rviz',
                                                 'launch/unity_slam_example.py',
-                                                'launch/unity_viz_example.py',
-                                                'launch/navigation_launch_maes.py',
-                                                'params/nav2_params_maes.yaml'
+                                                'launch/rviz_launch.py',
+                                                'launch/maes_bringup_launch.py',
+                                                'launch/maes_slam_launch.py',
+                                                'params/maes_nav2_multirobot_params_robot0.yaml',
+                                                'params/maes_nav2_multirobot_params_robot1.yaml',
+                                                'params/maes_nav2_multirobot_params_robot2.yaml',
                                                 ])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Unity Robotics',
-    maintainer_email='unity-robotics@unity3d.com',
-    description='Unity Robotics Nav2 SLAM Example',
+    maintainer='Maes',
+    maintainer_email='pholle17@student.aau.dk',
+    description='Maes Ros2 interface',
     license='Apache 2.0',
     tests_require=['pytest']
 )
