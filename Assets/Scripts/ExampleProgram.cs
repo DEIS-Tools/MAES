e6e1c8563aa26e2ee7396abc6ea7e2369088852b
magnus <magnjensen@gmail.com>
2022-05-10 16:02:16 +0200

    Added an example program that uses the new Simulator entry point.

--- Assets/Scripts/ExampleProgram.cs
+++ Assets/Scripts/ExampleProgram.cs
@@ -1,12 +1,19 @@
 using System;
+using Maes.Map.MapGen;
 using UnityEngine;
 
 namespace Maes {
     internal class ExampleProgram : MonoBehaviour {
         private void Start() {
+            // Get/instantiate simulation prefab
             var simulator = Simulator.GetInstance();
-            simulator.DefaultStart();
-            //simulator.StartSimulation();
+            
+            // Setup configuration for a scenario
+            var caveConfig = new CaveMapConfig(123, widthInTiles: 75, heightInTiles: 75);
+            var scenario = new SimulationScenario(123, mapSpawner: generator => generator.GenerateCaveMap(caveConfig));
+            simulator.EnqueueScenario(scenario);
+            
+            simulator.StartSimulation();
         }
     }
 }
