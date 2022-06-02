6623f2fb0b9842a3db8f9b01c3f455d2e9984df0
Magnus Kirkegaard Jensen <40539604+MagnusKJensen@users.noreply.github.com>
2022-06-02 11:03:52 +0200

    Save state removal (#119)
    * Removed ISavable interface and all traces of its methods.
    
    * Removed ISavable interface implementations
    
    * Added section to table of contents in readme
    
    * Fixed bug caused by internal keyword.
    
    * Made MeshGenerator internal.

--- Assets/Scripts/Map/MapGen/MapGenerator.cs
+++ Assets/Scripts/Map/MapGen/MapGenerator.cs
@@ -37,7 +37,11 @@ namespace Maes.Map.MapGen {
         public Transform innerWalls3D;
         public Transform wallRoof;
 
-        private MeshGenerator _meshGenerator;
+        internal MeshGenerator _meshGenerator;
+
+        private void Awake() {
+            _meshGenerator = GetComponent<MeshGenerator>();
+        }
 
         // Variable used for drawing gizmos on selection for debugging.
         private int[,] mapToDraw = null;
@@ -59,7 +63,7 @@ namespace Maes.Map.MapGen {
                 (bitMapHeight / 10f) + padding);
         }
 
-        internal void clearMap() {
+        public void clearMap() {
             _meshGenerator.ClearMesh();
         }
 
