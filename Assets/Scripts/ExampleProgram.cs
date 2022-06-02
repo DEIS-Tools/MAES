54e965a35171a08e1fcf2784a78f78c4bafbbae9
Malte Z. Andreasen <mzan16@student.aau.dk>
2022-06-02 11:14:45 +0200

    State of MAES repository at time of galactic-supported release.

--- Assets/Scripts/ExampleProgram.cs
+++ Assets/Scripts/ExampleProgram.cs
@@ -30,13 +30,7 @@ namespace Maes {
         private void Start() {
             // Get/instantiate simulation prefab
             var simulator = Simulator.GetInstance();
-            
-            // Setup configuration for a scenario
-            var caveConfig = new CaveMapConfig(123, widthInTiles: 75, heightInTiles: 75);
-            var scenario = new SimulationScenario(123, mapSpawner: generator => generator.GenerateCaveMap(caveConfig));
-            simulator.EnqueueScenario(scenario);
-        
-            simulator.PressPlayButton(); // Instantly enter play mode
+            simulator.DefaultStart(true);
         }
     }
 }
