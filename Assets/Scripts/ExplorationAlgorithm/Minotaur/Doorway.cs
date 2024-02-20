18c3cb2549d49dc55242f9d38ac70359af8c0dd1
Zzeeker <Rasmus.rbs@gmail.com>
2024-02-20 13:29:25 +0100

    Doorway Constructor

--- Assets/Scripts/ExplorationAlgorithm/Minotaur/Doorway.cs
+++ Assets/Scripts/ExplorationAlgorithm/Minotaur/Doorway.cs
@@ -11,6 +11,13 @@ namespace Maes.ExplorationAlgorithm.Minotaur
         public (Vector2, Vector2) Position;
         public bool Explored;
         public CardinalDirection.RelativeDirection ApproachedDirection;
+
+        public Doorway Doorway((Vector2, Vector2) Position, CardinalDirection.RelativeDirection ApproachedDirection)
+        {
+            this.Position = Position;
+            Explored = false;
+            this.ApproachedDirection = ApproachedDirection;
+        }
         
         public float Distance(MinotaurAlgorithm minotaur)
         {
