7385b2093429b8c35fbf759ff8fce09bb58a6c84
asa19aau <asa19@student.aau.dk>
2024-02-21 10:36:48 +0100

    initial commit

--- Assets/Scripts/Map/CoarseGrainedMap.cs
+++ Assets/Scripts/Map/CoarseGrainedMap.cs
@@ -342,9 +342,8 @@ namespace Maes.Map
         /// <summary>
         /// Calculates, and returns, a path from the robots current position to the target. Will reduce the path to a list of <see cref="PathStep"/>s.
         /// </summary>
-        public List<PathStep>? GetTnfPathAsPathSteps(Vector2Int target)
-        {
-            var path = GetPath(target, beOptimistic: true, acceptPartialPaths: true);
+        public List<PathStep>? GetTnfPathAsPathSteps(Vector2Int target) {
+            var path = GetPath(target, beOptimistic: false, acceptPartialPaths: false);
             return path == null
                 ? null
                 : _aStar.PathToSteps(path, 0f);
@@ -485,6 +484,34 @@ namespace Maes.Map
                 _slamMap.GetTileStatus(slamCoord + Vector2Int.up),
                 _slamMap.GetTileStatus(slamCoord + Vector2Int.up + Vector2Int.right),
             };
+        }
+
+        //Method for checking if a solid tile is actually solid, through the slam map
+        private SlamMap.SlamTileStatus IsOffsetSolid(Vector2Int nextCoordinate, Vector2Int currentCoordinate){
+            List<SlamMap.SlamTileStatus> slamStatuses = GetSlamTileStatuses(coordinate);
+            // s o o s
+            // s o o s ^
+            // s o o o |
+            // s o o o v
+            //
+            // o o o s
+            // o o o s
+            // s o o o <->
+            // s o o o
+            //
+            // o o s s
+            // o o o o ^
+            // o o o o |
+            // s s o o v
+            //
+            // s s s s
+            // o o o o
+            // o o o o <->
+            // s s o o
+            //
+            // may only need to check for diagonal movement
+
+            return SlamMap.SlamTileStatus.Solid
         }
 
         /// <summary>
