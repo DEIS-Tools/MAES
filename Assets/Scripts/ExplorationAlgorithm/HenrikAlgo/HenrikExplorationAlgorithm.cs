b2b848debe3b5338a28f3826f23b751b904685b0
Henrik van Peet <henrik_peet@hotmail.com>
2024-10-16 11:13:47 +0200

    refactor:destination reached detection

--- Assets/Scripts/ExplorationAlgorithm/HenrikAlgo/HenrikExplorationAlgorithm.cs
+++ Assets/Scripts/ExplorationAlgorithm/HenrikAlgo/HenrikExplorationAlgorithm.cs
@@ -1,15 +1,11 @@
 // Copyright 2024
 // Contributors: Henneboy
 
-using System;
-using System.Collections.Generic;
 using System.Text;
-using Codice.Client.Commands;
 using Maes.Map;
 using Maes.Robot;
 using Maes.Robot.Task;
 using UnityEngine;
-using Random = System.Random;
 
 namespace Maes.ExplorationAlgorithm.HenrikAlgo
 {
@@ -36,29 +32,29 @@ namespace Maes.ExplorationAlgorithm.HenrikAlgo
             var status = _robotController.GetStatus();
             if (status == RobotStatus.Idle)
             {
-                if (_robotController.IsCurrentlyColliding()){
-                    _robotController.Move(0.5f, true);
-                }
+                CollisionCorrector();
                 _targetTile = _robotController.GetSlamMap().CoarseMap.GetNearestTileFloodFill(_robotController.GetSlamMap().CoarseMap.GetCurrentPosition(), SlamMap.SlamTileStatus.Unseen);
             }
-            if (_targetTile != null){
-                if (_robotController.HasCollidedSinceLastLogicTick()){
-                    if (_robotController.GetSlamMap().CoarseMap.IsSolid(_targetTile.Value)){
-                        _targetTile = null;
-                        _robotController.StopCurrentTask();
-                        return;
-                    }
-                }
-                // Robot reached destination
-                if (_robotController.GetSlamMap().CoarseMap.GetCurrentPosition() == _targetTile.Value){
+            if (_targetTile != null)
+            {
+                _robotController.PathAndMoveTo(_targetTile.Value);
+                if (_robotController.GetSlamMap().CoarseMap.IsTileExplored(_targetTile.Value))
+                {
                     _targetTile = null;
                     _robotController.StopCurrentTask();
-
                     return;
                 }
-                _robotController.PathAndMoveTo(_targetTile.Value);
                 return;
+            }
+        }
 
+        private void CollisionCorrector()
+        {
+            if (_robotController.IsCurrentlyColliding())
+            {
+                _robotController.Move(0.2f, true);
+                _robotController.Rotate(45);
+                _robotController.Move(0.2f, true);
             }
         }
 
